7d8c4f3ab007cdb8ac1bbbeb24cf9bde677aff73
LoveMe <1872496619@qq.com>
2020-05-23 11:32:35 +0800

    添加了onload事件绑定和insertAfter函数

--- util/util.js
+++ util/util.js
@@ -3,3 +3,26 @@ function selectFrom(min, max) {
 	let choices = max - min;
 	return Math.floor(Math.random() * choices + min);
 }
+
+/** 绑定onload事件 */
+function addLoadEvent(func) {
+	let old = window.onload;
+	if(typeof old === 'function') {
+		window.onload = function() {
+			old();
+			func();
+	   }
+	} else {
+		window.onload = func;
+	}
+}
+
+/** 将元素插入指定元素的后面 */
+function insertAfter(newElement, targetElement) {
+	let parent = targetElement.parent;
+	if(parent.lastNode === targetElement) {
+		parent.appendChild(newElement);
+	} else {
+		parent.insertBefore(newElement, targetElement.nextSibling);
+	}
+}
