d056b5b863732dfc10e7c8b159a72b1949961aa3
LoveMe <1872496619@qq.com>
2020-05-24 14:11:10 +0800

    构造函数创建对象

--- oop/constructor-func.js
+++ oop/constructor-func.js
@@ -1,1 +1,11 @@
+/** 构造函数创建对象 */
+function Person() {
+	this.name = "coco";
+	this.age = 25;
+	this.sayName = function() {console.info(this.name + "你好")}
+}
 
+// use
+let a = new Person();
+let b = new Person();
+//缺点 ： 每个实例的行为也是一个独立的对象，这不符合OOP的思想。
