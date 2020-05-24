15ce635bc5b917ab64b8234107226b558bcb64be
LoveMe <1872496619@qq.com>
2020-05-24 14:07:11 +0800

    工厂模式创建对象

--- oop/factory.js
+++ oop/factory.js
@@ -1,1 +1,14 @@
 /** 工厂模式创建对象 */
+function createFactory() {
+	let o = {};
+	o.name = "coco";
+	o.age = 25;
+	o.sayHi = function() {
+		console.info(this.name + "你好");
+	}
+	return o;
+}
+
+// use
+let a = createFactory();
+let b = createFactory();
