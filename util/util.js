2f31e0c11a7eda7893dad4a6f2639e7d4d8a95dc
LoveMe <1872496619@qq.com>
2020-06-27 22:36:57 +0800

    update utils.js

--- util/util.js
+++ util/util.js
@@ -43,3 +43,25 @@ function getQueryStringArgs() {
 	}
 	return args;
 }
+
+/** 根据解决不同浏览器获取innerText的问题 */
+function getInnerText(element) {
+	return (typeof element.textContent == "string") ? element.textContent : element.innerText;
+}
+
+function setInnerText(element, text) {
+	if(typeof element.textContent == "string") {
+		element.textContent = text;
+        } else {
+		element.innerText = text;
+	}
+}
+
+
+
+
+
+
+
+
+
